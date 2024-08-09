9a5ca54d14465a650e63cf4c084cc1605ff54d67
Imran Kazi <imran.kazi@rysun.com>
2024-08-09 11:18:46 +0530

    updated dependencies, used node20.x feature

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import { glob } from "glob";
+import path from 'path';
 
 //this function check the console.log in all the existing files 
-const lintFile = (filePath: string) => {
-    fs.readFile(`./${filePath}`, "utf-8", (err, data) => {
-        try {
-            if (data?.includes("console.log(")) {
-                throw new Error(`${process.cwd()}/${filePath} contains console.log() please remove it.`)
-            }
-        } catch (error) {
-            console.error(error);
-            process.exit(1);
-        }
-    })
-}
+const lintFile = async (filePath: string) => {
+    try {
+        const fullPath = path.resolve(filePath); // Get absolute path
 
+        // Check if path is a directory
+        const stats = await fs.stat(fullPath);
+        if (!stats.isDirectory()) {         
+
+            // Read file contents
+            const data = await fs.readFile(fullPath, "utf-8");
+
+            // Check for forbidden content
+            if (data.includes("console.log(")) {
+                throw new Error(`${fullPath} contains console.log() please remove it.`);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
 const getFileNames = async () => {
     const data = await glob('**', { ignore: 'node_modules/**' })
     return data
